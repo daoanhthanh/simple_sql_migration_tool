8d0161eeab16afcd5744587c55e15fd03154ca95
Đào <thanh_da@flinters.vn>
2023-10-26 21:38:18 +0700

    added example

---
+++ scripts/example.sql
@@ -0,0 +1,8 @@
+insert into book (title, author, pages, price)
+values  ('gg', 'ghg', 6, 2.34),
+        ('hghg', 'ghg', 76, 3.4);
+
+
+insert into book (title, author, pages, price)
+values  ('gg', 'ghg', 6, 2.34),
+        ('hghg', 'ghg', 76, 3.4);
